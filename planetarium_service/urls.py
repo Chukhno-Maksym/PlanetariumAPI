6eb5ade81b31a0878ea7dac28ee2871a3d9046f8
Maksym <chukhno.maksym@gmail.com>
2025-02-20 20:25:15 +0200

    Create endpoints

--- planetarium_service/urls.py
+++ planetarium_service/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/planetarium", include("planetarium.urls"))
+    path("api/planetarium/", include("planetarium.urls"))
 ]
